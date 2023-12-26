7ac93b77d738c99054af93aa286b817e076b7caa
slava.lobikov <lobikova12@mail.ru>
2023-12-26 17:01:57 +0300

    add test

--- js/index.js
+++ js/index.js
@@ -3,20 +3,17 @@ let orientationSupport = !!window.DeviceOrientationEvent && !desktop
 document.addEventListener('mousemove', (e) => {
     document.documentElement.style.setProperty('--moveX', `${(e.clientX - window.innerWidth / 2) * -0.005}deg`)
     document.documentElement.style.setProperty('--moveY', `${(e.clientY - window.innerHeight /2) * -0.01}deg`)
-    /*Object.assign(document.documentElement, {
-        style: `
-            --moveX: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-            --moveY: ${(e.clientY - window.innerHeight /2) * -0.01}deg;
-        `
-    })*/
 })
 
 
 window.addEventListener('deviceorientation', handleDeviceOrientation);
 
 function handleDeviceOrientation(event) {
-    const moveX = (event.beta - 90) * -0.5;
-    const moveY = (event.gamma - 90) * -0.5;
+    let moveX = (event.beta - 90) * -0.5;
+    let moveY = (event.gamma - 90) * -0.5;
+
+    moveX = Math.max(Math.min(moveX, 3.5), -3.5);
+    moveY = Math.max(Math.min(moveY, 3.5), -3.5);
     if (orientationSupport) {
         document.documentElement.style.setProperty('--moveX', moveX + 'deg');
         document.documentElement.style.setProperty('--moveY', moveY + 'deg');
